f850dcfabb2ae24606f45213a371ea80656a0c59
Vladimir Puzakov <vppuzakov@gmail.com>
2019-10-16 03:53:59 +0300

    WIP: call deployment service from status form

--- gitlab_ui/app.py
+++ gitlab_ui/app.py
@@ -1,6 +1,9 @@
+import os
+
 from npyscreen import NPSAppManaged, setTheme
 from npyscreen.npysThemes import ElegantTheme
 
+from gitlab_ui.deployment_service import DeploymentService
 from gitlab_ui.status_form import StatusForm
 
 
@@ -9,4 +12,7 @@ class GitlabApp(NPSAppManaged):
     def onStart(self):
         setTheme(ElegantTheme)
 
-        self.registerForm("MAIN", StatusForm(name='DEV'))
+        host = os.environ.get('GITLAB_HOST', 'gitlab.com')
+        token = os.environ.get('GITLAB_TOKEN')
+        deployment_service = DeploymentService(host=host, token=token)
+        self.registerForm("MAIN", StatusForm(name='DEV', deployments=deployment_service))
--- gitlab_ui/status_form.py
+++ gitlab_ui/status_form.py
@@ -1,24 +1,25 @@
-import random
+from typing import List
 
-from npyscreen import Form, TitleText, FormMultiPage
+from npyscreen import FormMultiPage
 
 from gitlab_ui.color_grid import ColorGrid
+from gitlab_ui.deployment_service import DeploymentService
 
 
 class StatusForm(FormMultiPage):
 
-    def __init__(self, *args, **keywords) -> None:
+    def __init__(self, deployments: DeploymentService, projects: List[int], *args, **keywords) -> None:
+        self._deployments = deployments
+        self._projects = projects
         super().__init__(*args, **keywords)
 
     def create(self):
         gd = self.add(ColorGrid)
+        for project_id in self._projects:
+            deployments = self._deployments.get_project_deployments(project_id)
+            deployments_map = {deployment['env']: deployment for deployment in deployments}
 
-        gd.values = []
-        for x in range(3):
-            row = []
-            for y in range(4):
-                if bool(random.getrandbits(1)):
-                    row.append("PASS")
-                else:
-                    row.append("FAIL")
-            gd.values.append(row)
+            gd.values = []
+            for x in range(1):
+                row = [deployments_map.get('dev')]
+                gd.values.append(row)
